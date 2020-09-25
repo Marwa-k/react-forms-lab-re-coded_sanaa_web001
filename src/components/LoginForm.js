9bb2136d9ecf58895954fa3be97d733f5ad1ce88
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-25 06:08:53 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,26 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+  //  this.state = {};
+  this.state = {
+    username: "",
+    password: ""
+  };
+}
+
+handleInputChange = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
+handleSubmit = (event) => {
+  event.preventDefault()
+
+  if (!!this.state.username && !!this.state.password) {
+    this.props.handleLogin(this.state)
+  }
+}
   }
 
   render() {
